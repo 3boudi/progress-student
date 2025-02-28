6e0370e105632e8c30130c53a181c4882b5d3b46
3boudi <116286877+3boudi@users.noreply.github.com>
2025-02-28 11:20:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,57 +1,52 @@
 <?php
 session_start();
 require_once "db.php";
-if(isset($_POST['calc'])){
-    if($conn){
-        if(!empty($_POST['username']) && !empty($_POST['password'])){
-            $_SESSION['username'] = $_POST['username'];
-            $_SESSION['password'] = $_POST['password'];
-            header("Location:  index.php");
-
-        }
-    }
-
-}
-  if (isset($_POST['calc']) ) {
-
-$name = mysqli_real_escape_string($conn, $_POST['name']);
-$password = $_POST['password'];
-$sql = "SELECT * FROM etudents WHERE etudent = '$name'";
-$result = mysqli_query($conn, $sql);
-if ($result && mysqli_num_rows(($result))>0){
-
-    $row = mysqli_fetch_assoc($result);
-    if(password_verify($password, $row['password'])){
-        echo "<div class='output'>";
-        echo "<br>";
-        echo "STUDENT NAME:".$row['etudent'];
-        echo "<br>";
-        echo $row['moyen'];
-        if($row['moyen']<10){
-            echo "<br>";
-            echo '<p class="fail">Sorry, you failed!</p>';
-        }
-        else{
-            echo "<br>";
-            echo '<p class="success">Congratulations, you passed!</p>';
-        }
-        echo "</div>";
-    }
-    else{
-        echo "<div class='output'>";
-        echo "<br>";
-        echo '<p class="fail">Invalid password!</p>';
-        echo "</div>";
-    }
-}else{
-    echo "<div class='output'>";
-    echo '<p class="fail">User not found!</p>';
-    echo "</div>";
+if (isset($_SESSION['login_time']) && (time() - $_SESSION['login_time'] > 60)) {
+    session_unset();
+    session_destroy();
 }
 
-
+if (isset($_POST['calc'])) {
+    if ($conn) {
+        $name = mysqli_real_escape_string($conn, $_POST['name']);
+        $password = $_POST['password'];
+        $sql = "SELECT * FROM etudents WHERE etudent = '$name'";
+        $result = mysqli_query($conn, $sql);
+        if ($result && mysqli_num_rows($result) > 0) {
+            $row = mysqli_fetch_assoc($result);
+            if (password_verify($password, $row['password'])) {
+                $_SESSION['name'] = $name;
+                $_SESSION['password'] = $password;
+                $_SESSION['login_time'] = time();
+                echo "<div class='output'>";
+                echo "<br>";
+                echo "STUDENT NAME: " . $row['etudent'];
+                echo "<br>";
+                echo $row['moyen'];
+                if ($row['moyen'] < 10) {
+                    echo "<br>";
+                    echo '<p class="fail">Sorry, you failed!</p>';
+                } else {
+                    echo "<br>";
+                    echo '<p class="success">Congratulations, you passed!</p>';
+                }
+                echo "</div>";
+            } else {
+                echo "<div class='output'>";
+                echo "<br>";
+                echo '<p class="fail">Invalid password!</p>';
+                echo "</div>";
+            }
+        } else {
+            echo "<div class='output'>";
+            echo '<p class="fail">User not found!</p>';
+            echo "</div>";
+        }
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+    mysqli_close($conn);
 }
-mysqli_close($conn);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -63,7 +58,7 @@ mysqli_close($conn);
 </head>
 <body>
     <div class="container">
-        <h1>WELCOME STUDENT ENTER YOUR INFOMATION</h1>
+        <h1>WELCOME STUDENT ENTER YOUR INFORMATION</h1>
         <form id="usernote" action="" method="post">
             <div class="form-group">
                 <label for="name">Your Name:</label>
